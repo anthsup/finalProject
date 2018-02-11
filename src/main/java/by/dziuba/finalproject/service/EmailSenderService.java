ceee61aa668de24394adc380a19c4b559d60122d
anthonyer <anthsup@gmail.com>
2018-02-11 03:29:39 +0300

    reorganizing things a little

--- src/main/java/by/dziuba/finalproject/service/EmailSenderService.java
+++
@@ -1,5 +0,0 @@
-package by.dziuba.finalproject.service;
-
-public interface EmailSenderService {
-    boolean sendEmail(String message, String subject, String emailAddress);
-}
--- src/main/java/by/dziuba/finalproject/service/PasswordReminderService.java
+++
@@ -1,7 +0,0 @@
-package by.dziuba.finalproject.service;
-
-import by.dziuba.finalproject.service.exception.ServiceException;
-
-public interface PasswordReminderService {
-    void remindPassword(String email) throws ServiceException;
-}
